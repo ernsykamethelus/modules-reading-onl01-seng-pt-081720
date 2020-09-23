a8004256a76029845494060b413e54f95bd11e19
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-23 18:23:50 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -12,18 +12,3 @@ class Dancer
       end
 end
 
-angelina = Dancer.new
-angelina.twirl
-//returns "I'm twirling!"
-angelina.jump
-//returns "Look how high I'm jumping!"
- 
-buster = Kid.new
-buster.jump
-//returns "Look how high I'm jumping!"
-buster.take_a_bow
-//returns "Thank you, thank you. It was a pleasure to dance for you all."
-
-Dancer.metadata
-//returns "This class produces objects that love to dance."
-Kid.metadata
