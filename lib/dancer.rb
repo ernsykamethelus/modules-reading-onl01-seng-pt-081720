c92dc575ffa6fa201f720eb66983cfbb4da128f4
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-23 18:08:52 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,29 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+   extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+      attr_accessor :name 
+      
+      def initialize(name)
+        @name = name
+      end
+end
+
+angelina = Dancer.new
+angelina.twirl
+//returns "I'm twirling!"
+angelina.jump
+//returns "Look how high I'm jumping!"
+ 
+buster = Kid.new
+buster.jump
+//returns "Look how high I'm jumping!"
+buster.take_a_bow
+//returns "Thank you, thank you. It was a pleasure to dance for you all."
+
+Dancer.metadata
+//returns "This class produces objects that love to dance."
+Kid.metadata
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+    attr_accessor :name 
+    
+    def initialize (name)
+      @name = name
+    end
+end
+
+
+Dancer.metadata
+// returns "This class produces objects that love to dance."
+Kid.metadata
+// returns "This class produces objects that love to dance."
